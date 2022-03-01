a137935c396ab9b3b47038fa055684b318161214
KirienkoVS <kirienkov@yandex.ru>
2022-03-01 17:27:35 +0300

    Implements complication #2

--- app/src/main/java/com/example/androidintensive_5_fragments/MainActivity.kt
+++ app/src/main/java/com/example/androidintensive_5_fragments/MainActivity.kt
@@ -1,44 +1,78 @@
 package com.example.androidintensive_5_fragments
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentManager
 
 class MainActivity: AppCompatActivity(), ContactsFragment.ContactClickListener, ContactDetailsFragment.SaveButtonClickListener {
+
+    private lateinit var manager: FragmentManager
+    private var isTablet: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        if (supportFragmentManager.findFragmentByTag(CONTACTS_FRAGMENT_TAG) == null) {
-            supportFragmentManager.beginTransaction().run {
-                replace(R.id.fragment_container, ContactsFragment.newInstance(Bundle()), CONTACTS_FRAGMENT_TAG)
-                addToBackStack(CONTACTS_FRAGMENT_TAG)
+        manager = supportFragmentManager
+        isTablet = resources.getBoolean(R.bool.isTablet)
+
+        if (manager.findFragmentByTag(CONTACTS_FRAGMENT_TAG) == null) {
+            if (isTablet) {
+                manager.beginTransaction().run {
+                    add(R.id.contacts_fragment_container, ContactsFragment.newInstance(Bundle()), CONTACTS_FRAGMENT_TAG)
+                    commit()
+                }
+            } else {
+                manager.beginTransaction().run {
+                    replace(R.id.fragment_container, ContactsFragment.newInstance(Bundle()), CONTACTS_FRAGMENT_TAG)
+                    addToBackStack(CONTACTS_FRAGMENT_TAG)
+                    commit()
+                }
+            }
+        }
+
+    }
+
+    override fun onContactClicked(bundle: Bundle, contactID: String) {
+        if (isTablet) {
+            manager.beginTransaction()
+                .replace(R.id.contacts_details_fragment_container, ContactDetailsFragment.newInstance(bundle, contactID), CONTACT_DETAILS_FRAGMENT_TAG)
+                .commit()
+            findViewById<View>(R.id.contacts_details_fragment_container).visibility = View.VISIBLE
+        } else {
+            manager.beginTransaction().run {
+                replace(R.id.fragment_container,
+                    ContactDetailsFragment.newInstance(bundle, contactID),
+                    CONTACT_DETAILS_FRAGMENT_TAG
+                )
+                addToBackStack(CONTACT_DETAILS_FRAGMENT_TAG)
                 commit()
             }
         }
     }
 
-    override fun onContactClicked(bundle: Bundle, contactID: String) {
-        supportFragmentManager.beginTransaction().run {
-            replace(R.id.fragment_container, ContactDetailsFragment.newInstance(bundle, contactID), CONTACT_DETAILS_FRAGMENT_TAG)
-            addToBackStack(CONTACT_DETAILS_FRAGMENT_TAG)
-            commit()
-        }
-    }
-
     override fun onSaveButtonClicked(bundle: Bundle) {
-        supportFragmentManager.apply {
-            popBackStack()
-            beginTransaction().run {
-                replace(R.id.fragment_container, ContactsFragment.newInstance(bundle), CONTACTS_FRAGMENT_TAG)
+        if (isTablet) {
+            manager.beginTransaction().run {
+                replace(R.id.contacts_fragment_container, ContactsFragment.newInstance(bundle), CONTACTS_FRAGMENT_TAG)
                 commit()
+            }
+            findViewById<View>(R.id.contacts_details_fragment_container).visibility = View.GONE
+        } else {
+            manager.apply {
+                popBackStack()
+                beginTransaction().run {
+                    replace(R.id.fragment_container, ContactsFragment.newInstance(bundle), CONTACTS_FRAGMENT_TAG)
+                    commit()
+                }
             }
         }
     }
 
     override fun onBackPressed() {
-       if (supportFragmentManager.backStackEntryCount > 1) {
-           supportFragmentManager.popBackStack()
+       if (manager.backStackEntryCount > 1) {
+           manager.popBackStack()
        } else finish()
     }
 }
